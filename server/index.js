bd81a985577a6811ada07850391de54b7ddda0eb
James Scolamieri <james.scolamieri@gmail.com>
2021-02-12 11:38:29 -0800

    added express server and got app server running, ready for development

---
+++ server/index.js
@@ -0,0 +1,11 @@
+const express = require('express');
+const path = require('path');
+
+const port = 3000;
+const app = express();
+
+app.use('/', express.static(path.join(__dirname, '../public')));
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`);
+});
